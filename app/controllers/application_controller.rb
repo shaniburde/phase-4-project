4bc46a4998bc87ed67436c08b1e2201123551cf8
Jennifer Tchai <jentchai@gmail.com>
2022-03-07 21:49:56 -0500

    added some login/logout routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,15 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
-  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_reponse
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def current_user
+    User.find_by(id: session[:current_user])
+  end
+
+  def authorized_user
+      return render json: { error: "Not authorized" }, status: :unauthorized unless current_user
+  end
 
   private 
 
@@ -10,7 +18,7 @@ class ApplicationController < ActionController::API
       render json: { error: "#{error.model} Not Found" }, status: :not_found
   end
 
-  def render_unprocessable_entity_reponse(invalid)
+  def render_unprocessable_entity_response(invalid)
       render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   
+  # resources :sessions, only: [:create]
   resources :comments
   resources :yoga_poses, only: [:index, :show]
   resources :users, only: [:index, :create, :destroy]
+  post "/login", to: "sessions#login"
+  delete "/logout", to: "sessions#logout"
+  get '/authorized_user', to: 'users#show'
+  
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
